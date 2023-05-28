8386838dc88c08fe0b93abbe9c7a49227dd65de5
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-05-28 17:59:09 +0200

    fix

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 export function fetchCountries(name) {
   const fields = 'name,flags,capital,population,languages';
-  const encodedName = encodeURIComponent(name); // кодируем имя страны, чтобы использовать в URL
+  const encodedName = encodeURIComponent(name);
   const url = `https://restcountries.com/v3.1/name/${encodedName}?fields=${fields}`;
 
   return fetch(url)
